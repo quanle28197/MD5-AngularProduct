0daa5e68452488b163af97eb9109d4413e30ba92
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-05-29 21:03:44 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then(module => module.ProductModule)
+  },
+  {
+    path: 'category',
+    loadChildren: () => import('./category/category.module').then(module =>module.CategoryModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
